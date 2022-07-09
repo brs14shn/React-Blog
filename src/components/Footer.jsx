f363ef526e354fba6e2bbb27de842cca307a72b2
brs14shn <brsshn292929@gmail.com>
2022-07-10 00:21:12 +0300

    post && postpage updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   const today = new Date();
   return (
-    <footer>
+    <footer className="Footer">
       <p>Copyright &copy; {today.getFullYear()}</p>
     </footer>
   );
